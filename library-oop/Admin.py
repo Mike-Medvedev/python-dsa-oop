31110b43bd788b7d1fc2f504a550528e2dfee148
Mike Medvedev <mmedvedev20@gmail.com>
2025-02-14 23:47:03 -0500

    refreshing oop

---
+++ library-oop/Admin.py
@@ -0,0 +1,6 @@
+from User import User
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age):
+        super().__init__(first_name, last_name, age)
---
+++ library-oop/Book.py
@@ -0,0 +1,3 @@
+class Book:
+    def __init__(self, name, loan_period):
+        pass
